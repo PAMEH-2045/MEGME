42927f418c75dcba29f74d9e378c2f7bfbb14e6d
PAMEH-2045 <1239g.g.g1239@gmail.com>
2026-04-19 10:54:22 +0300

    remove unnecessary DefaultExecutionOrder

--- Scripts/Runtime/GestureManager.cs
+++ Scripts/Runtime/GestureManager.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 
 namespace BlackStartX.GestureManager
 {
-    [DefaultExecutionOrder(-10)] // if Module's LateUpdate() would be invoked after UniversalBlendshapes's LateUpdate() visemes wouldn't work
     public class GestureManager : MonoBehaviour
     {
         public const string Version = "Gesture Manager 3.9";
