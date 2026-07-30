b97c26149447b0b5d98a954bc05889db5f831528
PAMEH-2045 <1239g.g.g1239@gmail.com>
2026-07-30 15:36:53 +0300

    add model loading on drag and drop

---
+++ Drag'n'Dropper.cs
@@ -0,0 +1,33 @@
+﻿using Kirurobo;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace BlackStartX.GestureManager
+{
+    class Drag_n_Dropper : MonoBehaviour
+    {
+        UniWindowController controller;
+        VRMLoader loader;
+
+        string[] extentions = [".vrm", ".me", ".prefab", ".vrca"];
+
+        void Awake()
+        {
+            controller = FindFirstObjectByType<UniWindowController>();
+            loader = FindFirstObjectByType<VRMLoader>();
+        }
+        void OnEnable() => controller.OnDropFiles += OnDropFiles;
+        void OnDisable() => controller.OnDropFiles -= OnDropFiles;
+
+        void OnDropFiles(string[] files)
+        {
+            if (files.Length > 0 && !string.IsNullOrEmpty(files[0]))
+            {
+                var ext = Path.GetExtension(files[0]).ToLower();
+                if (extentions.Contains(ext))
+                    loader.LoadVRM(files[0]);
+            }
+        }
+    }
+}
